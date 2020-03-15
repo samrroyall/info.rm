875871cfef6bbfae208685a90c2d8ea2b2e5e187
samrroyall <samrroyall@gmail.com>
2020-03-14 19:13:13 -0500

    flask dashboard functionality done

--- info.rm.py
+++ info.rm.py
@@ -1,57 +1,122 @@
 #!/usr/bin/env python3
 
+import os
+import pathlib
+
 from flask import Flask, render_template
+import src.query
 
 #####################
 ### QUERY HELPERS ###
 #####################
 
-def generate_player_card(rank, player_name, team_name, logo_url, value):
-    return f"""<li class="list-group-item">
-          <div class="row h-100 justify-content-center align-items-center">
-            <div class="col-3 text-left float-left">
-              <img src="{logo_url}" height="30px"/>
-            </div>
-            <div class="col-6 text-left float-center">
-              <div class="row">
-                <strong>{rank}. {player_name}</strong>
-              </div>
-              <div class="row">
-                {team_name}
-              </div>
-            </div>
-            <div class="col-3 text-right float-right">
-              <strong>{value}</strong>
-            </div>
-          </div>
-        </li>
-    """
+file_path = pathlib.Path(__file__).parent.absolute()
+DB_PATH = os.path.join(str(file_path), "db/info.rm.db")
+
+def dashboard_stmt(stat, league, desc = True, additional_filter = None):
+    filter_list = [("players.minutes_played", ">", "900.0")]
+    if league is not None:
+        filter_list.append(("players.league_name", "=", league))
+    if additional_filter is not None:
+        filter_list.append(additional_filter)
+    filter_list = [(filter_list, "AND")]
+    stmt = src.query.Statement(
+        table_names = [("players", "team_id"), ("teams", "id")], 
+        select_fields = ["players.name", f"players.{stat}", "teams.name", "teams.logo"],
+        filter_fields = filter_list,
+        order_fields = ([f"players.{stat}"], desc)
+    )
+    return stmt
+
+def rank_result(query_result):
+    count = 0
+    rank = 0
+    prev_result = float("inf") 
+    for idx in range(len(query_result)):
+        tup = query_result[idx]
+        name = tup[0]
+        stat = float(tup[1])
+        team_name = tup[2]
+        team_logo = tup[3]
+        count += 1
+        if stat < prev_result:
+            rank = count
+        prev_result = stat
+        ranked_tup = {
+            "rank": rank,
+            "name": name,
+            "stat": stat,
+            "team_name": team_name,
+            "team_logo": team_logo
+        }
+        query_result[idx] = ranked_tup
+    return query_result
+
+def attacking_stats(league = None):
+    query_result = dict()
+    for stat in ["goals", "assists", "shots_on"]:
+        stmt = dashboard_stmt(stat, league)
+        query_result[stat] = rank_result(src.query.Query(DB_PATH, stmt).query_db())
+    return query_result
+
+def creation_stats(league = None):
+    query_result = dict()
+    for stat in ["passes_key", "dribbles_succeeded", "passes"]:
+        stmt = dashboard_stmt(stat, league)
+        query_result[stat] = rank_result(src.query.Query(DB_PATH, stmt).query_db())
+    return query_result
+
+def defending_stats(league = None):
+    query_result = dict()
+    for stat in ["tackles", "interceptions", "blocks"]:
+        stmt = dashboard_stmt(stat, league)
+        query_result[stat] = rank_result(src.query.Query(DB_PATH, stmt).query_db())
+    return query_result
+
+def goalkeeping_stats(league = None):
+    query_result = dict()
+    for stat in ["goals_conceded", "penalties_saved"]:
+        if stat == "goals_conceded":
+            stmt = dashboard_stmt(stat, league, False, ("players.position", "=", "Goalkeeper"))
+        else:
+            stmt = dashboard_stmt(stat, league, True, ("players.position", "=", "Goalkeeper"))
+        query_result[stat] = rank_result(src.query.Query(DB_PATH, stmt).query_db())
+    return query_result
+
+def dashboard_stats(league = None):
+    query_result = dict()
+    query_result["attacking"] = attacking_stats(league)
+    query_result["creation"] = creation_stats(league)
+    query_result["defending"] = defending_stats(league)
+    query_result["goalkeeping"] = goalkeeping_stats(league)
+    return query_result
+
 
 info_rm = Flask(__name__)
 
 @info_rm.route("/")
 def all_leagues():
-    return render_template("index.html")
+    return render_template("dashboard.html", query_result=dashboard_stats())
 
 @info_rm.route("/bundesliga")
 def bundesliga():
-    return render_template("index.html")
+    return render_template("dashboard.html", query_result=dashboard_stats("Bundesliga 1"))
 
 @info_rm.route("/ligue-1")
 def ligue_1():
-    return render_template("index.html")
+    return render_template("dashboard.html", query_result=dashboard_stats("Ligue 1"))
 
 @info_rm.route("/la-liga")
 def la_liga():
-    return render_template("index.html")
+    return render_template("dashboard.html", query_result=dashboard_stats("Primera Division"))
 
 @info_rm.route("/premier-league")
 def premier_league():
-    return render_template("index.html")
+    return render_template("dashboard.html", query_result=dashboard_stats("Premier League"))
 
 @info_rm.route("/serie-a")
 def serie_a():
-    return render_template("index.html")
+    return render_template("dashboard.html", query_result=dashboard_stats("Serie A"))
 
 if __name__ == "__main__":
     info_rm.run(debug=True)
--- src/query.py
+++ src/query.py
@@ -1,9 +1,10 @@
 import os
 import pathlib
 import sqlite3
-from __future__ import annotations
-from orm import Leagues, Teams, Players
 from typing import List, Tuple, Optional
+
+from .orm import Leagues, Teams, Players
+from .config import get_config_arg
 
 ###########################
 ###### Query Classes ######
@@ -19,20 +20,22 @@ class Filter:
             f"ERROR: Filter accepts following operations: {str(cls._OPS)[1:-1]}."
         assert len(field.split(".")) == 2, \
             "ERROR: Field table not specified."
-        self.check_properties(field, operator, value)
-        self.field = field
-        self.operator = operator
-        self.value = value
+        self.field, self.operator, self.value = self.check_properties(field, operator, value)
 
     def check_properties(self, field, operator, value) -> None:
-        table_strings = ["Leagues", "Teams", "Players"]
+        table_strings = ["leagues", "teams", "players"]
+        leagues = [league.split(",")[0] for league in eval(get_config_arg("leagues"))]
         field_table = field.split(".")[0]
         field_name = field.split(".")[1]
         assert field_table in table_strings, \
             "ERROR: Field table is incorrect."
-        expected_value_type = eval(field_table).get_type(field_name)
+        expected_value_type = eval(field_table.capitalize())().get_type(field_name)
         if expected_value_type == str:
-            assert value.isalpha(), "ERROR: Field value is incorrect."
+            if field_name == "league_name":
+                assert value in leagues, "ERROR: Invalid league name sent."
+            else:
+                assert value.isalpha(), "ERROR: Field value is incorrect."
+            value = f"\'{value}\'"
         elif expected_value_type == int:
             assert value.isdecimal(), "ERROR: Field value is incorrect."
         elif expected_value_type == float:
@@ -42,6 +45,7 @@ class Filter:
         elif expected_value_type == bool:
             assert value == "true" or value == "false", \
                 "ERROR: Field value is incorrect."
+        return field, operator, value
 
     def to_str(self) -> str:
         return f"{self.field} {self.operator} {self.value}"
@@ -68,61 +72,104 @@ class FilterList:
 
 class Order:
 
-    def __init__(self, order_fields: List[str]) -> None:
-        self.order_fields = order_fields
+    def __init__(
+            self, 
+            order_fields: Tuple[List[str], bool]
+        ) -> None:
+        self.order_fields, self.desc = order_fields
 
     def to_str(self) -> str:
-        return ", ".join(self.order_fields)
+        order_list = ", ".join(self.order_fields)
+        query_string = f" ORDER BY {order_list}"
+        if self.desc:
+            query_string += " DESC"
+        query_string += " LIMIT 50"
+        return query_string
 
 class Select:
 
-    def __init__(self, select_fields: List[str]) -> None:
+    def __init__(
+            self, 
+            select_fields: List[str],
+            table_names: List[Tuple[str,str]],
+        ) -> None:
         self.select_fields = select_fields
+        assert len(table_names) > 0,\
+            "ERROR: Statement class requires at least one table."
+        assert len(table_names) < 3,\
+            "ERROR: Statement class cannot take more than two tables."
+        if len(table_names) == 1:
+            self.table_names = [table_names[0][0]]
+        elif len(table_names) == 2:
+            assert table_names[0][0] != table_names[1][0], \
+                "ERROR: Cannot perform join on the same table."
+            self.table_names = [table_names[0][0], table_names[1][0]]
+            self.join_params = [table_names[0][1], table_names[1][1]]
+
+    def check_tables(self) -> None:
+        db_tables = ["leagues", "teams", "players"]
+        tables = self.table_names
+        for table in tables:
+            assert table in db_tables, "ERROR: Invalid table name supplied."
+        value_tables = list(set([column.split(".")[0] for column in self.select_fields]))
+        for table in value_tables:
+            assert table in tables, "ERROR: Invalid select param table name supplied"
 
     def to_str(self) -> str:
-        return ", ".join(self.select_fields)
+        select_list = ", ".join(self.select_fields)
+        self.check_tables()
+        if len(self.table_names) == 1:
+            query_string = f"SELECT {select_list} FROM {self.table_names[0]}"
+        else:
+            query_string = f"SELECT {select_list} FROM {self.table_names[0]} JOIN {self.table_names[1]}"
+            join_param_one = f"{self.table_names[0]}.{self.join_params[0]}"
+            join_param_two = f"{self.table_names[1]}.{self.join_params[1]}"
+            query_string += f" ON {join_param_one} = {join_param_two}"
+        return query_string
 
 
 class Statement:
 
     def __init__(
         self, 
-        table_name: str, 
+        table_names: List[Tuple[str,str]], 
         select_fields: List[str], 
         order_fields: Optional[List[str]], 
         filter_fields: Optional[List[Tuple[List[Tuple[str,str,str]], str]]]
     ) -> None:
-        self.table_name = table_name
-        self.select_fields = Select(select_fields)
+        self.select_stmt = Select(select_fields, table_names)
         if order_fields:
             self.order_fields = Order(order_fields)
         if filter_fields:
             self.filter_fields = [ FilterList(field) for field in filter_fields ]
 
     def to_str(self) -> str:
-        query_string = f"SELECT {self.select_fields.to_str()} FROM {self.table_name}"
+        query_string = self.select_stmt.to_str()
         if hasattr(self, "filter_fields"):
             filter_string = " AND ".join([ f"({filter.to_str()})" for filter in self.filter_fields ])
             query_string += f" WHERE {filter_string}"
         if hasattr(self, "order_fields"):
-            query_string += f" ORDER BY {self.order_fields.to_str()}"
+            query_string += f" {self.order_fields.to_str()}"
         return f"{query_string};"
+
 
 
 class Query:
 
     def __init__(self, db_path: str, statement: Statement) -> None:
-        if os.path.isfile(db_path) and os.path.splitext(db_path) == ".db":
-            self.db_path = db_path
-        else:
-            print("ERROR: Query class requires db_path to be a path to an existing sqlite DB.")
-        if isinstance(statement, Statement):
-            self.statement = statement
-        else:
-            print("ERROR: Query class requires statement to be an instance of the Statement class.")
+        assert os.path.isfile(db_path) and os.path.splitext(db_path)[1] == ".db",\
+            "ERROR: invalid DB path supplied to Query."
+        self.db_path = db_path
+        assert isinstance(statement, Statement),\
+            "ERROR: Query class not supplied with Statement object."
+        self.statement = statement
 
     def query_db(self) -> None:
         connection = sqlite3.connect(self.db_path)
         cursor = connection.cursor()
-
+        cursor.execute(self.statement.to_str())
+        query_result = cursor.fetchall()
+        connection.commit()
+        connection.close()
+        return query_result
 
