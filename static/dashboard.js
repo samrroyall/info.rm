3d3453db234d9d34119efbdf9d6c8f729b36d68a
Sam Royall <sroyall@STROZLLC.PUBLIC>
2020-03-23 17:45:23 +0100

    more mobile stuff

--- static/dashboard.js
+++ static/dashboard.js
@@ -1,3 +1,34 @@
+// Stat arrows JS
+function changeStat(stat, currPage, action) {
+  if (action == "next") {
+    $("#" + stat + "_card_" + currPage.toString()).addClass("hidden");
+    $("#" + stat + "_prev_" + currPage.toString()).addClass("hidden");
+    $("#" + stat + "_next_" + currPage.toString()).addClass("hidden");
+    if (currPage != 0) {
+      $("#" + stat + "_card_" + (currPage - 1).toString()).removeClass("hidden");
+      $("#" + stat + "_prev_" + (currPage - 1).toString()).removeClass("hidden");
+      $("#" + stat + "_next_" + (currPage - 1).toString()).removeClass("hidden");
+    } else { 
+      $("#" + stat + "_card_2").removeClass("hidden");
+      $("#" + stat + "_prev_2").removeClass("hidden");
+      $("#" + stat + "_next_2").removeClass("hidden");
+    }
+  } else if (action == "prev") {
+    $("#" + stat + "_card_" + currPage.toString()).addClass("hidden");
+    $("#" + stat + "_prev_" + currPage.toString()).addClass("hidden");
+    $("#" + stat + "_next_" + currPage.toString()).addClass("hidden");
+    if (currPage != 2) {
+      $("#" + stat + "_card_" + (currPage + 1).toString()).removeClass("hidden");
+      $("#" + stat + "_prev_" + (currPage + 1).toString()).removeClass("hidden");
+      $("#" + stat + "_next_" + (currPage + 1).toString()).removeClass("hidden");
+    } else {
+      $("#" + stat + "_card_0").removeClass("hidden");
+      $("#" + stat + "_prev_0").removeClass("hidden");
+      $("#" + stat + "_next_0").removeClass("hidden");
+    }
+  }
+}
+// Pagination JS
 function changePage(stat, substat, page) {
   var i = 1;
   for (; i < 6; i++) {
@@ -10,3 +41,23 @@ function changePage(stat, substat, page) {
      }
   }
 }
+// Per 90 Toggler JS
+$(document).ready( function() {
+  $(function () {var split_loc = window.location.href.split("/");
+    if (split_loc[split_loc.length - 1] == "per-90") {
+      $("#main_per90_toggle").prop("checked", true);
+      $("#main_per90_toggle_label").toggleClass("text-secondary text-primary");
+    }
+  });
+  $(function() {
+    $("#main_per90_toggle").change(function () {
+      $("#main_per90_toggle_label").toggleClass("text-secondary text-primary");
+      var split_loc = window.location.href.split("/");
+      if (split_loc[split_loc.length - 1] == "per-90") {
+        window.location.href = window.location.href.split("/per-90")[0];
+      } else {
+        window.location.href = window.location.href + "/per-90";
+      }
+    });
+  });
+});
