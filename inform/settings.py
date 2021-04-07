ededbad946a83a1ab547fb1dcb71c068bf3917f6
Sam Royall <samrroyall@gmail.com>
2021-04-07 23:35:00 +0100

    removed Django SECRET KEY

--- inform/settings.py
+++ inform/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b!em&()xn(t+6^ef%2&5ekj*!95=+alrxlp%k)6_bfa2b*o%wn'
+SECRET_KEY = os.environ.get("SECRET_KEY"); 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
